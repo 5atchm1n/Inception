a73f073c74aa222890c745bbbe2b1e7a36cc7ad3
sshakya <sshakya@student.42.fr>
2022-05-31 16:33:19 +0200

    ftp_up

--- srcs/bonus/ftp_server/conf/setup.sh
+++ srcs/bonus/ftp_server/conf/setup.sh
@@ -1,1 +1,16 @@
-!#/bin/sh
+#!/bin/sh
+
+if [ ! -f .setup ] ;
+    then
+        echo "Adding FTP user password"
+        echo "$FTP_USER:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
+        
+
+        echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+
+        touch .setup
+        rm setup.sh
+fi
+
+echo "FTP started on port 21"
+/usr/sbin/vsftpd    /etc/vsftpd/vsftpd.conf
